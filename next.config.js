582084124143b2a91250b3570f11cfe5e3a07882
huy tran <tranthehuy.2nd@gmail.com>
2025-02-12 00:46:55 +0700

    Config static deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  
-  reactStrictMode: true,
-  // if we want to build a server files, plese remove output and basePath attributes
+
+const env = process.env.NEXT_CONFIG_ENV || 'development';
+
+const nextConfigs = env === 'development' ? {
+  reactStrictMode: true
+} : {
   output: 'export',
-  basePath: "/material-ui-nextjs-pages-router-ts"
+  basePath: "/material-ui-nextjs-pages-router-ts",
+  reactStrictMode: true
 };
+
+module.exports = nextConfigs 
